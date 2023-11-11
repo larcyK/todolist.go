4dbf216ad16d8d92884db5b8c93d7d3f2c247355
larcyK <40230524+larcyK@users.noreply.github.com>
2023-11-12 04:58:12 +0900

    add task-edit-function

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
 /* placeholder file for JavaScript */
+const confirm_delete = (id) => {
+    if(window.confirm(`Task ${id} を削除します．よろしいですか？`)) {
+        location.href = `/task/delete/${id}`;
+    }
+}
+ 
+const confirm_update = (id) => {
+    if(window.confirm(`Task ${id} を更新します．よろしいですか？`)) {
+        document.getElementsByTagName('form')[0].submit();
+    }
+}
